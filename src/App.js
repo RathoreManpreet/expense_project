39e18d15411e4516019221405279b5d0dbede77d
Manpreet Sing <itmanpreet94@gmail.com>
2021-07-08 21:52:40 +0530

    initial work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Expenses from './components/Expense/Expenses';
+import React from 'react';
 
 function App() {
+  const expenses = [
+    {
+      date: new Date(2021, 7, 8),
+      title: 'Car Insurance',
+      price: 2000,
+    },
+    {
+      date: new Date(2021, 6, 13),
+      title: 'Home Insurance',
+      price: 2000,
+    },
+    {
+      date: new Date(2021, 2, 8),
+      title: 'Education Insurance',
+      price: 2000,
+    },
+
+  ];
+  // return React.createElement(
+  //   'div',
+  //   { className: 'App' },
+  //   React.createElement(Expenses, { items: expenses })
+  // )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Expenses items={expenses} />
     </div>
   );
 }
